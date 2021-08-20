2c1b7219e7fbb5c0f51acaa89102a68b2549ffbf
Emmanuel Ndubuisi <psynch45@gmail.com>
2021-08-20 16:39:00 -0400

    Create august-18-class-exercise.R

---
+++ class-exercise/august-18-class-exercise.R
@@ -0,0 +1,10 @@
+# Author: Emmanuel Ndubuisi, Date: 08/18/2021
+
+x = rnorm(10)
+y = rnorm(10)
+
+pts = seq(-4.5,4.5,length=100)
+plot(pts,dt(pts,df=9),col='red',type='l')
+
+lines(density(x), col='green')
+lines(density(y), col='blue')
