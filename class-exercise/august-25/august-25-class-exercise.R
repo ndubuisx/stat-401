9da2a8cdea84fe29398bd7bb97ba26401ca88b6c
Emmanuel Ndubuisi <psynch45@gmail.com>
2021-08-25 16:08:41 -0400

    Create august-25-class-exercise.R

---
+++ class-exercise/august-25/august-25-class-exercise.R
@@ -0,0 +1,6 @@
+# Author: Emmanuel Ndubuisi, Date: August 25, 2021, Purpose: --
+
+if(!require(devtools)) install.packages("devtools")
+devtools::install_github("kassambara/ggpubr")
+
+library("ggpubr")
