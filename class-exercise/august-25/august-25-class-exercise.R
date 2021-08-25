7d131747f9d34bb395bbb85df13855ce72bfbc71
Emmanuel Ndubuisi <psynch45@gmail.com>
2021-08-25 16:23:36 -0400

    Update august-25-class-exercise.R

--- class-exercise/august-25/august-25-class-exercise.R
+++ class-exercise/august-25/august-25-class-exercise.R
@@ -3,4 +3,13 @@
 if(!require(devtools)) install.packages("devtools")
 devtools::install_github("kassambara/ggpubr")
 
+# load library
 library("ggpubr")
+
+# get sample data 
+my_data = mtcars
+
+# get + plot correlation value
+# conf.int = TRUE, cor.coef = TRUE (these assignments would show the corr. value on the plot)
+# cor.method = "pearson" (what correlation method to use)
+ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
