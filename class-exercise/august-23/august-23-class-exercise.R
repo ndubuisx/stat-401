407742d39e5f27658d5189bb7af256f6b18d637d
Emmanuel Ndubuisi <psynch45@gmail.com>
2021-08-25 16:06:56 -0400

    Create august-23-class-exercise.R

---
+++ class-exercise/august-23/august-23-class-exercise.R
@@ -0,0 +1,6 @@
+# Author: Emmanuel Ndubuisi, Date: August 23, 2021, Purpose: Loading ggpubr library.
+
+if(!require(devtools)) install.packages("devtools")
+devtools::install_github("kassambara/ggpubr")
+
+library("ggpubr")
