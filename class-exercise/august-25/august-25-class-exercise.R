994bd04eb9758d99b312f55b6a27c2900668fddb
Emmanuel Ndubuisi <psynch45@gmail.com>
2021-08-25 16:26:11 -0400

    Update august-25-class-exercise.R

--- class-exercise/august-25/august-25-class-exercise.R
+++ class-exercise/august-25/august-25-class-exercise.R
@@ -11,5 +11,6 @@ my_data = mtcars
 
 # get + plot correlation value
 # conf.int = TRUE, cor.coef = TRUE (these assignments would show the corr. value on the plot)
+# R is the coefficient value
 # cor.method = "pearson" (what correlation method to use)
 ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
