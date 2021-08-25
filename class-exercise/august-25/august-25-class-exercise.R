de36a26a14e37668c58e3445130d54e06a2d45ae
Emmanuel Ndubuisi <psynch45@gmail.com>
2021-08-25 16:10:57 -0400

    Update august-25-class-exercise.R

--- class-exercise/august-25/august-25-class-exercise.R
+++ class-exercise/august-25/august-25-class-exercise.R
@@ -1,4 +1,4 @@
-# Author: Emmanuel Ndubuisi, Date: August 25, 2021, Purpose: --
+# Author: Emmanuel Ndubuisi, Date: August 25, 2021, Purpose: Calculate correlation coefficient value.
 
 if(!require(devtools)) install.packages("devtools")
 devtools::install_github("kassambara/ggpubr")
