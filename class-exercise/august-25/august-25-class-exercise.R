b9db726b4e94ec0a56649168f7cd8b613a0106d1
Emmanuel Ndubuisi <psynch45@gmail.com>
2021-08-25 16:28:56 -0400

    Update august-25-class-exercise.R

--- class-exercise/august-25/august-25-class-exercise.R
+++ class-exercise/august-25/august-25-class-exercise.R
@@ -14,3 +14,7 @@ my_data = mtcars
 # R is the coefficient value
 # cor.method = "pearson" (what correlation method to use)
 ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+
+# generates correlation value without plotting
+# my_data$wt => the dollar sign is used to select the column name (my_data$wt = weight column, my_data$mpg = mpg column)
+res = cor.test(my_data$wt, my_data$mpg, method = "pearson")
