b31714f0ab272c8391be6369ffdb2f4a671af0f1
Emmanuel Ndubuisi <psynch45@gmail.com>
2021-10-08 16:41:59 -0400

    Create october-8-class-exercise.R

---
+++ class-exercise/october-8/october-8-class-exercise.R
@@ -0,0 +1,8 @@
+# Author: Emmanuel Ndubuisi, Date: October 8, 2021, Purpose: Create a simple linear regression model
+
+# Question: Test if happiness increases with facebook usage or vice-versa.
+# x = independent variable - facebook usage
+x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
+
+# y = dependent variable (or outcome) - happiness ranked on scale 0 - 10
+y <- c(1, 2, 9, 3, 4, 5, 7, 2, 2, 8)
