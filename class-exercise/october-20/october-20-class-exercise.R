83cb3b222240b545a65e8ffb3711e174b839bfac
Emmanuel Ndubuisi <10370951+ndubuisx@users.noreply.github.com>
2021-10-22 16:10:08 -0400

    Create october-20-class-exercise.R

---
+++ class-exercise/october-20/october-20-class-exercise.R
@@ -0,0 +1,13 @@
+# Author: Emmanuel Ndubuisi, Date: October 20, 2021, Purpose: Perform multiple linear regression on sample data
+
+install.packages("tidyverse")
+install.packages("datarium")
+
+# load tidyverse library
+library(tidyverse)
+
+# load datarium library
+library(datarium)
+
+# load the sample data from tinyverse
+data("marketing", package="datarium")
