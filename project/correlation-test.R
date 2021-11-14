cd11ad125578408d96c51e4a4a62efb77c0e4b58
Emmanuel Ndubuisi <10370951+ndubuisx@users.noreply.github.com>
2021-11-14 01:33:04 -0500

    Update correlation-test.R

--- project/correlation-test.R
+++ project/correlation-test.R
@@ -8,24 +8,17 @@ data <- data %>% na_if("") %>% na.omit
 
 glimpse(data)
 
-# get + plot correlation value
-# conf.int = TRUE, cor.coef = TRUE (these assignments would show the corr. value on the plot)
-# R is the coefficient value
-# cor.method = "pearson" (what correlation method to use)
-ggscatter(data, x = "cigarette_sales", y = "death_rate", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Cigarettes Sales per Adult per Day", ylab = "Lung Cancer Mortality Rate per 100,000 people")
+ggscatter(data, x = "cigarette_sales", y = "new_cases", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "spearman", xlab = "Cigarettes Sales per Adult per Day", ylab = "Lung Cancer Rate per 100,000 people")
 
-# generates correlation value without plotting
-# my_data$wt => the dollar sign is used to select the column name (my_data$wt = weight column, my_data$mpg = mpg column)
-# in the output, 'cor' is the correlation value
-res = cor.test(data$cigarette_sales, data$death_rate, method = "pearson")
+res = cor.test(data$cigarette_sales, data$new_cases, method = "spearman")
 
-# Pearson's product-moment correlation
+print(res)
+
+# Spearman's rank correlation rho
 # 
-# data:  data$cigarette_sales and data$death_rate
-# t = 0.16798, df = 38, p-value = 0.8675
-# alternative hypothesis: true correlation is not equal to 0
-# 95 percent confidence interval:
-#  -0.2867024  0.3358988
+# data:  data$cigarette_sales and data$new_cases
+# S = 8981.4, p-value = 0.3319
+# alternative hypothesis: true rho is not equal to 0
 # sample estimates:
-#        cor 
-# 0.02723973 
+#       rho 
+# 0.1574626 
