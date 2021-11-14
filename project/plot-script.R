37930b6fcaf4be81f22c7aa6963ae93c6ab71f72
Emmanuel Ndubuisi <10370951+ndubuisx@users.noreply.github.com>
2021-11-14 01:36:48 -0500

    Update plot-script.R

--- project/plot-script.R
+++ project/plot-script.R
@@ -10,11 +10,10 @@ PATH <- "https://raw.githubusercontent.com/ndubuisx/stat-401/main/project/cigare
 data <- read.csv(PATH)
 
 # value used to transform the data
-coeff <- 6
+coeff <- 7
 
-# color for data plot line
 cigarette_sales_color <- "#0047AB"
-death_rate_color <- "#D22B2B"
+new_cases_color <- "#D22B2B"
 
 # preview data set
 glimpse(data)
@@ -22,7 +21,7 @@ glimpse(data)
 # plot data frame
 ggplot(data, aes(x=year)) + 
 geom_line( aes(y=cigarette_sales), size=1, color=cigarette_sales_color) + 
-geom_line( aes(y=death_rate / coeff), size=1, color=death_rate_color) +
-scale_y_continuous(name = "Sales of Cigarettes per Adult per Day", sec.axis = sec_axis(~.*coeff, name="Lung Cancer Mortality Rate per 100,000 people")) + 
-theme(axis.title.y = element_text(color = cigarette_sales_color, size=13), axis.title.y.right = element_text(color = death_rate_color, size=13)) +
-ggtitle("Cigarette Sales and Lung Cancer Mortality Rate in the United States.")
+geom_line( aes(y=new_cases / coeff), size=1, color=new_cases_color) +
+scale_y_continuous(name = "Sales of Cigarettes per Adult per Day", sec.axis = sec_axis(~.*coeff, name="Lung Cancer Rate per 100,000 people")) + 
+theme(axis.title.y = element_text(color = cigarette_sales_color, size=13), axis.title.y.right = element_text(color = new_cases_color, size=13)) +
+ggtitle("Cigarette Sales and Lung Cancer Rate in the United States.")
