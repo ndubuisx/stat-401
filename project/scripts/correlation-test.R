3ad09b41451a178aad925773ec5aa862dc65bed9
Ndubuisi, Emmanuel <10370951+ndubuisx@users.noreply.github.com>
2021-11-26 17:04:00 -0500

    update scripts and plots

--- project/scripts/correlation-test.R
+++ project/scripts/correlation-test.R
@@ -1,20 +1,32 @@
 library("ggpubr")
 
-PATH <- "https://raw.githubusercontent.com/ndubuisx/stat-401/main/project/cancer-cigarette.csv"
+PATH <- "https://raw.githubusercontent.com/ndubuisx/stat-401/main/project/data/cancer-cigarette.csv"
 data <- read.csv(PATH)
 
+# remove rows with empty cells
 data <- data %>% na_if("") %>% na.omit
 
+# preview data
 glimpse(data)
 
+# scatter plot cigarette_sales and mortality_rate_men
 ggscatter(data, x = "cigarette_sales", y = "mortality_rate_men", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Cigarettes Sales per Adult per Day", ylab = "Lung Cancer Mortality Rate per 100,000 Males")
 
-res = cor.test(data$cigarette_sales, data$mortality_rate_men, method = "pearson")
+# scatter plot cigarette_sales and mortality_rate_women
+ggscatter(data, x = "cigarette_sales", y = "mortality_rate_women", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Cigarettes Sales per Adult per Day", ylab = "Lung Cancer Mortality Rate per 100,000 Females")
 
-print(res)
+# pearson correlation test for cigarette_sales and mortality_rate_men
+cor_test_men = cor.test(data$cigarette_sales, data$mortality_rate_men, method = "pearson")
 
-# Pearson's product-moment correlation
-# 
+# pearson correlation test for cigarette_sales and mortality_rate_women
+cor_test_women = cor.test(data$cigarette_sales, data$mortality_rate_women, method = "pearson")
+
+print(cor_test_men)
+print(cor_test_women)
+
+### Correlation Test - Cigarette Sales and Male Lung Cancer Mortality Rate ###
+
+# 	Pearson's product-moment correlation
 # data:  data$cigarette_sales and data$mortality_rate_men
 # t = 1.1991, df = 80, p-value = 0.234
 # alternative hypothesis: true correlation is not equal to 0
@@ -23,3 +35,16 @@ print(res)
 # sample estimates:
 #       cor 
 # 0.1328699 
+
+
+### Correlation Test - Cigarette Sales and Female Lung Cancer Mortality Rate ###
+
+# 	Pearson's product-moment correlation
+# data:  data$cigarette_sales and data$mortality_rate_women
+# t = -4.2845, df = 80, p-value = 5.061e-05
+# alternative hypothesis: true correlation is not equal to 0
+# 95 percent confidence interval:
+#  -0.5933925 -0.2372516
+# sample estimates:
+#        cor 
+# -0.4320162 
